b77b2d6b718494909fdb7cc399c73ce2280e0b99
narip <narip@github.com>
2024-04-20 17:16:05 +0900

    feat: vite 개발서버 & 테스트 설정, 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+/// <reference types="vitest" />
+import { vitePlugin as remix } from '@remix-run/dev';
+import { installGlobals } from '@remix-run/node';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 installGlobals();
 
 export default defineConfig({
   plugins: [remix(), tsconfigPaths()],
+  test: {
+    globals: true,
+  },
+  server: {
+    port: 3000,
+    /** 포트가 이미 사용 중일 때 다른 포트를 사용하게끔 설정 */
+    strictPort: false,
+  },
 });
